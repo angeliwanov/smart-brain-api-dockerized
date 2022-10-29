114083fe4276c212b226fad6b09bae16a28f65b4
angeliwanov <angel.iwanov@gmail.com>
2022-10-29 07:03:46 +0300

    handleProfileUpdate

--- controllers/profile.js
+++ controllers/profile.js
@@ -11,6 +11,23 @@ const handleProfileGet = (req, res, db) => {
     .catch(err => res.status(400).json('user not found'))    
 }
 
+const handleProfileUpdate = (req, res, db) => {
+    const {id} = req.params;
+    const {name, age, pet} = req.body.formInput;
+    db('users')
+    .where({id})
+    .update({name: name})
+    .then(resp => {
+         if(resp) {
+            res.json("success")
+         }else {
+            res.status(400).json('unable to update')
+         }
+    })
+    .catch(err => res.status(400).json('error updating users '))
+}
+
 module.exports = {
-    handleProfileGet: handleProfileGet
+    handleProfileGet: handleProfileGet,
+    handleProfileUpdate: handleProfileUpdate
 }
--- server.js
+++ server.js
@@ -8,16 +8,6 @@ const profile = require('./controllers/profile')
 const image = require('./controllers/image')
 const morgan = require('morgan');
 
-// process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
-
-// const db = knex({
-//     client: 'pg',
-//     connection: {
-//       connectionString : process.env.DATABASE_URL,
-//       ssl : true,
-//     }
-// }); 
-
 const db = knex({
     client: 'pg',
     connection: process.env.POSTGRES_URI
@@ -33,10 +23,10 @@ app.get('/', (req, res) => {
     res.send('success')
 })
 
-
 app.post('/signin', (req, res) => {signin.handleSignin (req, res, db, bcrypt)})
 app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt)})
 app.get('/profile/:id', (req, res) => {profile.handleProfileGet(req, res, db)})
+app.post('/profile/:id', (req, res) => {profile.handleProfileUpdate(req, res, db)})
 app.put('/image', (req, res) => {image.handleImage(req, res, db)})
 app.post('/imageurl', (req, res) => {image.handleApiCall(req, res)})
 
